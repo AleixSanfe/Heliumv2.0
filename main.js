32dc88ef70d84522c63518fce4422f00696b4df1
SanfeBCN <aleix.sanfeliu@gmail.com>
2017-04-06 20:48:21 +0200

    SetUp

---
+++ main.js
@@ -0,0 +1,14 @@
+var app = require('app');  // Module to control application life.
+
+app.on('window-all-closed', function() {
+  if (process.platform != 'darwin' && app.active_windows.length == 0) {
+    app.quit();
+  }
+});
+
+const HMI = require(__dirname + '/bin/HMI.js');
+
+app.on('ready', function() {
+
+  HMI.new_window('chrome');
+});
